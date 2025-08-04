45c1aa2d208892a5938892d20bff01f36a2f8e55
MIKO SETIAWAN <98937432+mikosetiawan@users.noreply.github.com>
2025-08-04 21:09:09 +0700

    updated feature update

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\SubBidangController;
 use App\Http\Controllers\WaktuPelaksanaanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
